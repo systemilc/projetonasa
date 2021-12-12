547b7723c4779c60d0a54e71dda7b0e1f5ab92b2
systemilc <isaac.systemilc@gmail.com>
2021-12-12 11:01:08 -0300

    adicionado google tradutor e reset css

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 
-            $('#buscar').click(
-            function(){
-                let data = $('#buscaData').val()
-               console.log(buscaData)
-            }
-        
-        )
-
-
-// $('buscar').click(
-//     function(){
-//         let data = $('buscaData').val()
-//         $.ajax({
-//             URL: `https://api.nasa.gov/planetary/apod?api_key=nY1cr97W2x0xU9oZsjtJI6UJPi18QW4kFwPRA3b8/${data}`,
-//             type: 'GET',
-//             success: function(resultado){
-//                 console.log(resultado)
-        
-                
-//             }
-//         })
-//     }
-
-// )
-
+$("#buscar").click(
+    function () {
+        let data = $("#buscaData").val();
+        $.ajax({url: `https://api.nasa.gov/planetary/apod?api_key=nY1cr97W2x0xU9oZsjtJI6UJPi18QW4kFwPRA3b8&date=${data}`,
+        type: "GET",
+        success: function (resultado)
+        {console.log(resultado);
+            $("#dataFoto").text(`Foto adicionada em: ${resultado.date}`);
+            $("#titulo").text(resultado.title);
+            $("#texto").text(resultado.explanation);
+            $("#foto").attr("src", resultado.url);
+            // $('#autor').text(resultado.copyright)
+        },
+    })
+})
 
