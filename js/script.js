7e9d0a59fd8b67520e30b6aa9ae347177e5ddee8
systemilc <isaac.systemilc@gmail.com>
2021-12-12 13:25:45 -0300

    adicionado formatacao cabeçalho css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 
-$("#buscar").click(
+$('#buscar').click(
     function () {
-        let data = $("#buscaData").val();
+        let data = $('#buscaData').val();
         $.ajax({url: `https://api.nasa.gov/planetary/apod?api_key=nY1cr97W2x0xU9oZsjtJI6UJPi18QW4kFwPRA3b8&date=${data}`,
-        type: "GET",
+        type: 'GET',
         success: function (resultado)
-        {console.log(resultado);
-            $("#dataFoto").text(`Foto adicionada em: ${resultado.date}`);
-            $("#titulo").text(resultado.title);
-            $("#texto").text(resultado.explanation);
-            $("#foto").attr("src", resultado.url);
-            // $('#autor').text(resultado.copyright)
+        {
+            $('#dataFoto').text(`Foto adicionada em: ${resultado.date}`);
+            $('#titulo').text(resultado.title);
+            $('#texto').text(resultado.explanation);
+            $('#foto').attr('src', resultado.url);
+            $('#foto').attr('alt', `Titulo da foto: ${resultado.title}`);
+            $('#autor').text(resultado.copyright)
         },
     })
 })
