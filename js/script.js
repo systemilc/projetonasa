8ded0da66fa703b17fafbbf27137808c681a9a3a
systemilc <isaac.systemilc@gmail.com>
2021-12-11 22:23:50 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+            $('#buscar').click(
+            function(){
+                let data = $('#buscaData').val()
+               console.log(buscaData)
+            }
+        
+        )
+
+
+// $('buscar').click(
+//     function(){
+//         let data = $('buscaData').val()
+//         $.ajax({
+//             URL: `https://api.nasa.gov/planetary/apod?api_key=nY1cr97W2x0xU9oZsjtJI6UJPi18QW4kFwPRA3b8/${data}`,
+//             type: 'GET',
+//             success: function(resultado){
+//                 console.log(resultado)
+        
+                
+//             }
+//         })
+//     }
+
+// )
+
+
