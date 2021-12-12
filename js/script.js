ac3f2702a606f3c37d6978b37366658abe3d1d08
systemilc <isaac.systemilc@gmail.com>
2021-12-12 15:10:15 -0300

    alterado a font de todo o site - adicionado condicao if na validacao da data - adicionado mais estilizacoes ao css

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 $('#buscar').click(
     function () {
         let data = $('#buscaData').val();
+        if(data >='2020-06-20'){
+        
         $.ajax({url: `https://api.nasa.gov/planetary/apod?api_key=nY1cr97W2x0xU9oZsjtJI6UJPi18QW4kFwPRA3b8&date=${data}`,
         type: 'GET',
         success: function (resultado)
@@ -11,8 +13,12 @@ $('#buscar').click(
             $('#texto').text(resultado.explanation);
             $('#foto').attr('src', resultado.url);
             $('#foto').attr('alt', `Titulo da foto: ${resultado.title}`);
-            $('#autor').text(resultado.copyright)
+            $('#autor').text(`Autor: ${resultado.copyright}`)
         },
     })
+}else{7
+    $('#titulo').text('Erro: A primeira foto postada no APOD foi dia 20/06/2020');
+    
+}
 })
 
